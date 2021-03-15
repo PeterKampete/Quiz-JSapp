1c602f6631d10f380c95684d2ee09c0d0e1fad89
PeterKampete <peterkampete01@gmail.com>
2021-03-16 00:31:52 +0100

    first commit

---
+++ src/components/Result.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Result = ({ score, playAgain }) => {
+  return (
+    <div className="score-board">
+      <div className="score-board">You scored {score} / 5 correct answers!</div>
+      <button className="playBtn" onClick={playAgain}>
+        {" "}
+        Play again!
+      </button>
+    </div>
+  );
+};
+
+export default Result;
